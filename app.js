ae1531232a1d712e61bc7f46a3d67be2c96e4287
felix <felix@mou.uclm.es>
2015-07-13 12:48:24 +0200

    Despliegue en Heroku

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
@@ -15,6 +17,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -25,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
